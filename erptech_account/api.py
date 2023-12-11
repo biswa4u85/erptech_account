a644a11979b2439f5a1e1caf03a12656a2801c35
Biswajit Sahoo <biswa4u85@gmail.com>
2023-12-11 13:08:26 +0530

    add files

--- erptech_account/api.py
+++ erptech_account/api.py
@@ -4,6 +4,21 @@ from frappe import _
 @frappe.whitelist()
 def get_workspace_sidebar_items():
 	pages = [{
+        "name": "Home",
+        "title": "Home",
+        "for_user": "",
+        "parent_page": "",
+        "content": "[{\"id\":\"MmUf9abwxg\",\"type\":\"onboarding\",\"data\":{\"onboarding_name\":\"Home\",\"col\":12}}]",
+        "public": 1,
+        "module": "Home",
+        "icon": "Home",
+        "indicator_color": "red",
+        "is_hidden": 0,
+        "label": "Home",
+        "is_editable": True,
+        "selected": True
+    },
+    {
         "name": "Accounting",
         "title": "Accounting",
         "for_user": "",
@@ -17,6 +32,6 @@ def get_workspace_sidebar_items():
         "label": "Accounting",
         "is_editable": True,
         "selected": True
-    },
+    }
     ]
 	return {"pages": pages, "has_access": True}
